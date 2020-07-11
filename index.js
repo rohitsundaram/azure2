b8690322740eda110cf986df893beac465b7d53f
rohitsundaram <rohitsundaram.95@gmail.com>
2020-07-11 12:51:19 +0530

    fifteh commit

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
+const express = require('express');
+const app = express();
 const http = require('http');
-
-const server = http.createServer((request, response) => {
-    response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello World!");
+const hostname = '127.0.0.1';
+//const port = 3000;
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World rohit\n');
 });
 
-const port = process.env.PORT || 1337;
+const port = process.env.PORT || 1338;
 server.listen(port);
 
 console.log("Server running at http://localhost:%d", port);
